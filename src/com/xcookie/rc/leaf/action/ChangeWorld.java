668dd81146bfb7fdbcb084683ab50d79690d8bb1
Brendan Edser <bmedser@gmail.com>
2019-08-02 01:31:51 +1000

    world hop fix

--- src/com/xcookie/rc/leaf/action/ChangeWorld.java
+++ src/com/xcookie/rc/leaf/action/ChangeWorld.java
@@ -1,12 +1,18 @@
 package com.xcookie.rc.leaf.action;
 
 import com.runemate.game.api.hybrid.local.hud.interfaces.WorldHop;
+import com.runemate.game.api.script.Execution;
 import com.runemate.game.api.script.framework.tree.LeafTask;
 
 public class ChangeWorld extends LeafTask {
     @Override
     public void execute() {
-        WorldHop.hopTo(327);
+
+        if(!WorldHop.isOpen()) {
+            WorldHop.open();
+            Execution.delayUntil(WorldHop::isOpen);
+            WorldHop.hopTo(327);
+        }
 
     }
 }
